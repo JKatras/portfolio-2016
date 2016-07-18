8aa2979a0c496091098ee40ef406dfa309a3a1b4
Jeremy Katras <jakatras@gmail.com>
2016-07-18 16:30:16 -0400

    base files, switching links

---
+++ app.js
@@ -0,0 +1,24 @@
+$(function() {
+  var navTop = $('#nav-wrap').offset().top;
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > navTop && $(window).width() > 450) {
+      $('#nav-wrap').addClass('sticky');
+    } else {
+      $('#nav-wrap').removeClass('sticky');
+    }
+  });
+  if ($(window).width() < 450) {
+    $('#nav-wrap').addClass('sticky-mobile');
+  }
+  $(window).on('resize', function() {
+    if ($(window).width() < 450) {
+      $('#nav-wrap').addClass('sticky-mobile');
+    } else {
+      $('#nav-wrap').removeClass('sticky-mobile');
+    }
+  });
+
+  $('.show-mobile').click(function() {
+    $('#nav-wrap').toggleClass('hide-mobile');
+  });
+});
