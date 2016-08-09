773b9cf527eecb9d02ef91fb1b98dbcb3c95c886
Jeremy Katras <jakatras@gmail.com>
2016-08-09 15:54:37 -0400

    nav media query adjust

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 $(function() {
   var navTop = $('#nav-wrap').offset().top;
   $(window).scroll(function() {
-    if ($(window).scrollTop() > navTop && $(window).width() > 450) {
+    if ($(window).scrollTop() > navTop && $(window).width() > 500) {
       $('#nav-wrap').addClass('sticky');
     } else {
       $('#nav-wrap').removeClass('sticky');
     }
   });
-  if ($(window).width() < 450) {
+  if ($(window).width() < 500) {
     $('#nav-wrap').addClass('sticky-mobile');
   }
   $(window).on('resize', function() {
-    if ($(window).width() < 450) {
+    if ($(window).width() < 500) {
       $('#nav-wrap').addClass('sticky-mobile');
     } else {
       $('#nav-wrap').removeClass('sticky-mobile');
